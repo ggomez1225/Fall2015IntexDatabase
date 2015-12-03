7781fe5979d7372ced9121ef0f632bc954fb1942
asmockler <asmockler@gmail.com>
2015-12-02 21:15:24 -0700

    Indentation, etc.

--- NorthWestLabsDatabase.sql
+++ NorthWestLabsDatabase.sql
@@ -61,105 +61,124 @@ CREATE TABLE WorkOrder (
   InvoiceID int references Invoice not null
 );
 
-Create Table Invoice(
-InvoiceID int default next value for NWLID Sequence Primary Key,
-DueDate date,
-EarlyPaymentDate date,
-EarlyPaymentDiscount decimal (2,2),
-WorkOrderID int references WorkOrder not null);
+CREATE TABLE Invoice (
+  InvoiceID int default next value for NWLID Sequence Primary Key,
+  DueDate date,
+  EarlyPaymentDate date,
+  EarlyPaymentDiscount decimal (2,2),
+  WorkOrderID int references WorkOrder not null
+);
 
-Create Table Compound(
-LTNumber char(6) Primary Key,
-Name varchar(50),
-MolecularMass decimal(4,6));
+CREATE TABLE Compound (
+  LTNumber char(6) Primary Key,
+  Name varchar(50),
+  MolecularMass decimal(4,6)
+);
 
-Create Table CompoundSample(
-CompoundSampleID int default next value for NWLID
-Sequence Primary Key,
-CompoundSequenceCode int,
-QtyInMilligrams int
-DateArrived Date,
-DateDue Date,
-Appearance varchar(1000),
-ClaimedWeight decimal(4,6),
-ActualWeight decimal(4,6),
-MaximumToleratedDose decimal(4,6)
-LTNumber char references Compound not null,
-WorkOrderID int references WorkOrder not null,
-EmployeeID int references Employee not null);
+CREATE TABLE CompoundSample (
+  CompoundSampleID int default next value for NWLID
+  Sequence Primary Key,
+  CompoundSequenceCode int,
+  QtyInMilligrams int
+  DateArrived Date,
+  DateDue Date,
+  Appearance varchar(1000),
+  ClaimedWeight decimal(4,6),
+  ActualWeight decimal(4,6),
+  MaximumToleratedDose decimal(4,6)
+  LTNumber char references Compound not null,
+  WorkOrderID int references WorkOrder not null,
+  EmployeeID int references Employee not null
+);
 
-Create Table Assay(
-AssayID int default next value for NWLID Sequence Primary Key,
-Name varchar(150),
-Protocol varchar(3000),
-DaysToCompletion int);
+CREATE TABLE Assay (
+  AssayID int default next value for NWLID Sequence Primary Key,
+  Name varchar(150),
+  Protocol varchar(3000),
+  DaysToCompletion int
+);
 
-Create Table Test(
-TestID int default next value for NWLID Sequence Primary Key,
-Name varchar(50),
-Concentration decimal(4,6));
+CREATE TABLE Test (
+  TestID int default next value for NWLID Sequence Primary Key,
+  Name varchar(50),
+  Concentration decimal(4,6)
+);
 
-Create Table ConditionalTest(
-ConditionalTestID int references Test(TestID) Primary Key);
+CREATE TABLE ConditionalTest (
+  ConditionalTestID int references Test(TestID) Primary Key
+);
 
-Create Table RequiredTest(
-RequiredTestID int references Test(TestID) Primary Key);
+CREATE TABLE RequiredTest (
+  RequiredTestID int references Test(TestID) Primary Key
+);
 
-Create Table OptionalTest(
-TriggerCondition varchar(3000),
-ConditionalTestID int references ConditionalTest not null,
-RequiredTestID int references RequiredTest not null,
-Constraint OptionalTest_PK Primary Key(ConditionalTestID, RequiredTestID));
+CREATE TABLE OptionalTest (
+  TriggerCondition varchar(3000),
+  ConditionalTestID int references ConditionalTest not null,
+  RequiredTestID int references RequiredTest not null,
+  Constraint OptionalTest_PK Primary Key(ConditionalTestID, RequiredTestID)
+);
 
-Create Table AssayTest(
-AssayID int references Assay not null,
-TestID int references Test not null,
-Constraint AssayTest_PK Primary Key(AssayID, TestID));
+CREATE TABLE AssayTest (
+  AssayID int references Assay not null,
+  TestID int references Test not null,
+  Constraint AssayTest_PK Primary Key(AssayID, TestID)
+);
 
-Create Table LiteratureReference(
-ReferenceID int default next NWLID Sequence Primary Key,
-Name varchar(50),
-Notes varchar(50)
-AssayID int references Assay not null);
+CREATE TABLE LiteratureReference (
+  ReferenceID int default next NWLID Sequence Primary Key,
+  Name varchar(50),
+  Notes varchar(50)
+  AssayID int references Assay not null
+);
 
-Create Table Material(
-MaterialID int default next NWLID Sequence Primary Key,
-Name varchar(200),
-Cost decimal(4,2));
+CREATE TABLE Material (
+  MaterialID int default next NWLID Sequence Primary Key,
+  Name varchar(200),
+  Cost decimal(4,2)
+);
 
-Create Table TestMaterial(
-Quantity decimal(4,2),
-TestID int references Test not null,
-MaterialID int references Material not null,
-Constraint TestMaterial_PK Primary Key(TestID, MaterialID));
+CREATE TABLE TestMaterial (
+  Quantity decimal(4,2),
+  TestID int references Test not null,
+  MaterialID int references Material not null,
+  Constraint TestMaterial_PK Primary Key(TestID, MaterialID)
+);
 
-Create Table SampleTest(
-Hours decimal(3,2),
-CompoundSampleID int references CompoundSample not null,
-TestID int references Test not null,
-EmployeeID int references Employee not null,
-Constraint SampleTest_PK Primary Key(CompoundSampleID, TestID));
+CREATE TABLE SampleTest (
+  Hours decimal(3,2),
+  CompoundSampleID int references CompoundSample not null,
+  TestID int references Test not null,
+  EmployeeID int references Employee not null,
+  Constraint SampleTest_PK Primary Key(CompoundSampleID, TestID)
+);
 
-Create Table AssayResult(
-AssayResultID int default next NWLID Sequence Primary Key,
-AssayID int references Assay not null);
+CREATE TABLE AssayResult (
+  AssayResultID int default next NWLID Sequence Primary Key,
+  AssayID int references Assay not null
+);
 
-Create Table TestResult(
-TestResultID int default next MWLID Sequence Primary Key,
-TestID int references Test not null,
-AssayResultID int references AssayResult not null);
+CREATE TABLE TestResult (
+  TestResultID int default next MWLID Sequence Primary Key,
+  TestID int references Test not null,
+  AssayResultID int references AssayResult not null
+);
 
-Create Table File(
-FileID int default next NWLID Sequence Primary Key,
-FileURL varchar(150),
-TestResultID int references TestResult not null);
+CREATE TABLE File (
+  FileID int default next NWLID Sequence Primary Key,
+  FileURL varchar(150),
+  TestResultID int references TestResult not null
+);
 
-Create Table QualitativeResultFile(
-QualitativeResultFileID int references File(FileID) Primary Key);
+CREATE TABLE QualitativeResultFile (
+  QualitativeResultFileID int references File(FileID) Primary Key
+);
 
-Create Table QuantitativeResultFile(
-QuantitativeResultFileID int references File(FileID) Primary Key);
+CREATE TABLE QuantitativeResultFile (
+  QuantitativeResultFileID int references File(FileID) Primary Key
+);
 
-Create Table Report(
-ReportID int references File(FileID) Primary Key,
-WorkOrderID int references WorkOrder not null);
+CREATE TABLE Report (
+  ReportID int references File(FileID) Primary Key,
+  WorkOrderID int references WorkOrder not null
+);
